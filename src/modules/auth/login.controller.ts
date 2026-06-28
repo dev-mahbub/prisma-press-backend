ad0ea2a06de5e98515e05d94ba205975e8677837
Mahbub Hasan <easycodermahbubwith5500@gmail.com>
2026-06-28 20:42:20 +0600

    token set in cookie

--- src/modules/auth/login.controller.ts
+++ src/modules/auth/login.controller.ts
@@ -7,13 +7,27 @@ import httpStatus from "http-status";
 const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const result = await authService.loginUserService(payload);
+    const { accessToken, refreshToken } =
+      await authService.loginUserService(payload);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.CREATED,
       message: "User login successfully!",
-      data: result,
+      data: { accessToken, refreshToken },
     });
   },
 );
