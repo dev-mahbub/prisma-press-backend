0ebc4824ca1d6226e9e4a951b8f65e29f723a823
Mahbub Hasan <easycodermahbubwith5500@gmail.com>
2026-06-30 15:19:42 +0600

    user route and controller updated

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,9 +3,6 @@ import { userService } from "./user.service";
 import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import config from "../../config";
-import { jwtUtils } from "../../utils/jwt";
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -24,14 +21,9 @@ const registerUser = catchAsync(
 
 const getMyProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { accessToken } = req.cookies;
-
-    const verifiedToken = jwtUtils.verifyToken(
-      accessToken,
-      config.jwt_access_secret,
-    ) as JwtPayload;
-
-    const profile = await userService.getMyProfileFromDB(verifiedToken.id);
+    const profile = await userService.getMyProfileFromDB(
+      req.user?.id as string,
+    );
 
     sendResponse(res, {
       success: true,
@@ -41,28 +33,6 @@ const getMyProfile = catchAsync(
     });
   },
 );
-
-// const registerUser = async (req: Request, res: Response) => {
-//   try {
-//     const payload = req.body;
-
-//     const user = await userService.registerUserIntoDB(payload);
-
-//     res.status(httpStatus.CREATED).json({
-//       success: true,
-//       statusCode: httpStatus.CREATED,
-//       message: "User registerd successfully",
-//       data: { user },
-//     });
-//   } catch (error) {
-//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-//       success: false,
-//       statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-//       message: "Failed to register user!",
-//       error: (error as Error).message,
-//     });
-//   }
-// };
 
 export const userController = {
   registerUser,
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,58 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
+import { jwtUtils } from "../../utils/jwt";
+import config from "../../config";
+import { JwtPayload } from "jsonwebtoken";
+import { Role } from "../../../generated/prisma/enums";
+import httpStatus from "http-status";
 
 const router = Router();
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        name: string;
+        email: string;
+        role: Role;
+      };
+    }
+  }
+}
+
 router.post("/register", userController.registerUser);
-router.get("/me", userController.getMyProfile);
+router.get(
+  "/me",
+  (req: Request, res: Response, next: NextFunction) => {
+    const { accessToken } = req.cookies;
+
+    const verifiedToken = jwtUtils.verifyToken(
+      accessToken,
+      config.jwt_access_secret,
+    ) as JwtPayload;
+
+    const { id, name, email, role } = verifiedToken;
+
+    const requiredRoles = [Role.ADMIN, Role.AUTHOR, Role.USER];
+
+    if (!requiredRoles.includes(role)) {
+      res.status(httpStatus.BAD_REQUEST).json({
+        success: false,
+        statusCode: httpStatus.BAD_REQUEST,
+        message: "You don't have permission to access this resources",
+      });
+    }
+
+    req.user = {
+      id,
+      name,
+      email,
+      role,
+    };
+    next();
+  },
+  userController.getMyProfile,
+);
 
 export const userRouter = router;
