d5c00cca778d386c89c7aeea054ccdb8dc3c4a32
Mahbub Hasan <easycodermahbubwith5500@gmail.com>
2026-07-01 11:44:05 +0600

    refreshed token update access token

--- src/modules/auth/login.controller.ts
+++ src/modules/auth/login.controller.ts
@@ -32,6 +32,29 @@ const loginUser = catchAsync(
   },
 );
 
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const result = await authService.refreshTokenService(refreshToken);
+
+    res.cookie("accessToken", result, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Refresh token successfully create access token",
+      data: result,
+    });
+  },
+);
+
 export const authController = {
   loginUser,
+  refreshToken,
 };
--- src/modules/auth/login.service.ts
+++ src/modules/auth/login.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginPayload } from "./login.interface";
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { jwtUtils } from "../../utils/jwt";
 
@@ -40,6 +40,39 @@ const loginUserService = async (payload: ILoginPayload) => {
   return { accessToken, refreshToken };
 };
 
+const refreshTokenService = async (refreshToken: string) => {
+  const verifiedToken = jwtUtils.verifyToken(
+    refreshToken,
+    config.jwt_refresh_secret,
+  );
+
+  if (!verifiedToken.success) {
+    throw new Error(verifiedToken.error);
+  }
+
+  const { id } = verifiedToken.data as JwtPayload;
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id },
+  });
+
+  const jwtPayload = {
+    id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  return { accessToken };
+};
+
 export const authService = {
   loginUserService,
+  refreshTokenService,
 };
